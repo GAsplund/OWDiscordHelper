ea4330d35254eb263598b9c65525e5a25e6480b9
GAsplund <gasplund2@gmail.com>
2017-07-30 18:52:29 +0200

    More updates woo
    Changed this:
    Now showing how many users that have been handled
    Now auto creates text files if they are empty (but doesn't make any info
    in them)
    Now actually continues in code instead of hanging

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace DiscordOWHelper
 {
     class Program
     {
+        public static long userNo = 1;
+        public static string[] infoText = {"Basic info about my bot:","", "Config file: First line is your bot token, Second line is your guild/server ID, Third line is the debug setting.", "", "Example of config file:", "","", "(Very secret bot token);", "176689618194792448;", "false;","","", "Users file: Each line is one user, and looks like this:", "","","<BATTLETAG NAME>-<BATTLETAG ID>,<DISCORD USER ID>,<DISCORD NAME/NICK>;","", "", "Example of users file:", "", "", "L33tHax0r-1337,233892072606662658,Good Username;", "Bert-65536,233892072606662658,Unnamed;"};
+
+
         static void Main(string[] args)
         {
             Console.Title = "GAsplund's Overwatch Discord Helper";
@@ -23,20 +27,42 @@ namespace DiscordOWHelper
         {
             string line = "";
             string configFile = "";
+            if (!File.Exists("users.txt"))
+            {
+                File.Create("users.txt");
+            }
+            if (!File.Exists("info.txt"))
+            {
+                File.Create("info.txt");
+                //File.WriteAllLines("info.txt", infoText);
+            }
+            if (!File.Exists("config.txt"))
+            {
+                File.Create("config.txt");
+            }
             try
-            {   // Open the text file using a stream reader.
-                using (StreamReader sr0 = new StreamReader("users.txt"))
+            { // Getting the users file
+                using (StreamReader usersStream = new StreamReader("users.txt"))
                 {
-                    line = sr0.ReadToEnd();
-                }
-                using (StreamReader sr1 = new StreamReader("config.txt"))
-                {
-                    configFile = sr1.ReadToEnd();
+                    line = usersStream.ReadToEnd();
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Users file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            { // Getting the config file
+                using (StreamReader configStream = new StreamReader("config.txt"))
+                {
+                    configFile = configStream.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Users file could not be read:");
                 Console.WriteLine(e.Message);
             }
             string[] config = configFile.Split(';');
@@ -83,7 +109,7 @@ if (debug) { Console.WriteLine("Now getting and setting user data..."); };
 
             Console.WriteLine("Now getting and setting data...");
 
-            for (i = 0; i <= lineCount; i++) {
+            for (i = 0; i <= lineCount-1; i++) { // Getting and setting user data
                 string userdat = usrdata[i];
                 string[] udata = userdat.Split(',');
 
@@ -91,22 +117,23 @@ if (debug) { Console.WriteLine("Now getting and setting user data..."); };
                 ulong userID = Convert.ToUInt64(udata[1]);
                 string username = udata[2];
 
-if (debug) { Console.WriteLine("BattleTag: " + battleTag + " userID: " + userID + " username: " + username); };
+if (debug) { Console.WriteLine("User no. " + userNo + " with BattleTag: " + battleTag + " userID: " + userID + " username: " + username); };
 if (debug) { Console.WriteLine("Now getting web data..."); }
-
+                userNo++;
                 var clientResponse = await httpClient.GetAsync("https://owapi.net/api/v3/u/" + battleTag + "/stats");
                 var returnData = await clientResponse.Content.ReadAsStringAsync();
                 string source = returnData;
 if (debug) { Console.WriteLine("Parsing data for " + username); };
                 dynamic info = JObject.Parse(source);
 
-                long level = info.eu.stats.quickplay.overall_stats.prestige * 100 + info.eu.stats.quickplay.overall_stats.level;
+                long level = info.eu.stats.quickplay.overall_stats.prestige * 100 + 1 + info.eu.stats.quickplay.overall_stats.level;
                 Console.WriteLine(username + "'s level is " + level);
                 await client.ModifyMember(Convert.ToUInt64(config[1]), userID, nickname = username + " [Lv. " + level + "]");
                 await Task.Delay(1000);
             };
 
             string successMessage = "Successfully updated levels!";
+            Console.WriteLine("");
             Console.WriteLine(successMessage);
             //await client.SendMessage(319473403725873153, successMessage);
 
